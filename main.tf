9d8fa4e4b053b2bf30847588e13977993ce63948
balraamyadav <1998balluyadav@gmail.com>
2026-09-19 20:59:57 +0530

    lab-09-terraform-state

--- main.tf
+++ main.tf
@@ -9,34 +9,16 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_network" "app_network" {
-  name = "terraform-lifecycle-network"
-}
-
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
 
 resource "docker_container" "nginx" {
-  name  = "terraform-lifecycle-nginx"
+  name  = "terraform-state-nginx"
   image = docker_image.nginx.image_id
-
-  networks_advanced {
-    name = docker_network.app_network.name
-  }
 
   ports {
     internal = 80
     external = 8080
   }
-
-  lifecycle {
-    create_before_destroy = true
-
-    #prevent_destroy       = true
-  }
-
-  depends_on = [
-    docker_network.app_network
-  ]
 }
--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "container_id" {
   value = docker_container.nginx.id
 }
 
-output "network_name" {
-  value = docker_network.app_network.name
-}
-
-output "nginx_url" {
-  value = "http://localhost:8080"
+output "image_name" {
+  value = docker_image.nginx.name
 }
